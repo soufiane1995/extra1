abe47e2d20791f2ac44ba50bc2149a6d8f40b253
Soufiane <soufiane@muslim.com>
2020-02-25 10:00:02 +0100

    Clase del examen clasico. version: 1.6 Signed-off-by: Soufiane <soufiane@hackermail.com>

--- Extra1/src/notas/ExamenClasico.java
+++ Extra1/src/notas/ExamenClasico.java
@@ -1,23 +1,43 @@
+/**
+ * <h1>Clase Examen Clasico</h1>
+ * 
+ * @author Alvaro y Soufiane
+ * @version 1.6
+ * 
+ */
 package notas;
 
-public class ExamenClasico extends NotasAlumnos {
+import java.util.Scanner;
 
-	public static void examenClasico() {
+public class ExamenClasico {
+	public static float[] notas;
+	public static Scanner stdin = new Scanner(System.in);
+
+	public static void notasExamenClasico() {
 		System.out.println("		Exámenes clasicos");
 		for (int i = 0; i < 3; i++) {
-				System.out.print("Ingrese la nota del exámen " + (i + 1) + " : ");
-				notas[i] = stdin.nextFloat();
+			// pedir la nota al usuario.
+			System.out.print("Ingrese la nota del exámen " + (i + 1) + " : ");
+			notas[i] = stdin.nextFloat();
 
+			// mensaje de error. si la nota introducida es valida.
 			if (nota_valeda(notas[i])) {
 				System.err.println("\n			Error!!\n	Ingrese una nota válida (del 1 al 10)\n");
 				--i;
 			}
 		}
 	}
-	
-	public static float examen_clasico() {
+
+	// Clcular la nota media.
+	public static float mediaExamenClasico() {
 		float nota_examen_clasico = 0;
-		nota_examen_clasico = (notas[0] * 10 / 100) + (notas[1] * 20 / 100) + (notas[2] * 20 / 100);
+		nota_examen_clasico = ((notas[1] + notas[2]) * 20 + notas[0] * 10) / 100;
+		// (notas[0] * 10 / 100) + (notas[1] * 20 / 100) + (notas[2] * 20 / 100);
 		return nota_examen_clasico;
+	}
+
+	// valicar la nota, Comprovar si la nota esta entre 0 y 10.
+	public static boolean nota_valeda(float nota) {
+		return (nota <= 10 && nota >= 0) ? false : true;
 	}
 }
